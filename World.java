75cdc9e8b1b217bc1b385bf98939de3fd5d8d8bc
Kim, Arin <kialeen0806@gmail.com>
2025-09-17 16:59:21 +0900

    four

--- World.java
+++ World.java
@@ -1,1 +1,6 @@
-public class World { public static void run() { System.out.println("202314096"); } }
+public class World { 
+    public static void run() { 
+        System.out.println("202314096"); 
+        System.out.println("hello world");
+    } 
+}
